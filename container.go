932a5bd2eb1f281a11c6022dd5f73032e81c176f
Damien Szczyt <dszczyt@work4labs.com>
2022-07-22 19:35:55 +0200

    use arguments

--- container.go
+++ container.go
@@ -18,10 +18,27 @@ func (c *Container) Bind(src interface{}, dst interface{}) {
 	c.data[reflect.TypeOf(src)] = reflect.ValueOf(dst)
 }
 
-func Call[T any, F func(T)](c *Container, f F) {
+func Call1[T any, F func(T)](c *Container, f F) {
 	v := reflect.ValueOf(f)
 
 	args := []reflect.Value{c.data[reflect.PointerTo(v.Type().In(0))]}
+
+	v.Call(args)
+}
+
+func Call2[T any, U any, F func(T, U)](c *Container, f F, others ...any) {
+	v := reflect.ValueOf(f)
+
+	args := []reflect.Value{}
+
+	for i := 0; i < 2; i++ {
+		if arg, ok := c.data[reflect.PointerTo(v.Type().In(i))]; ok {
+			args = append(args, arg)
+		} else {
+			args = append(args, reflect.ValueOf(others[0]))
+			others = others[1:]
+		}
+	}
 
 	v.Call(args)
 }
--- di_test.go
+++ di_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/dszczyt/lady"
+	tassert "github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -35,7 +36,28 @@ func TestDI(t *testing.T) {
 			d.Demo()
 		}
 
-		lady.Call(container, test)
+		lady.Call1(container, test)
+
+		obj.AssertExpectations(t)
+	})
+
+	t.Run("simple with arg", func(t *testing.T) {
+		assert := tassert.New(t)
+
+		container := lady.New()
+
+		obj := DemoStruct{}
+
+		obj.On("Demo").Return()
+
+		container.Bind(new(DemoIface), &obj)
+
+		test := func(d DemoIface, s string) {
+			assert.Equal("good", s)
+			d.Demo()
+		}
+
+		lady.Call2(container, test, "good")
 
 		obj.AssertExpectations(t)
 	})
